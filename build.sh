c4795e0045eb0f6d5cfdc7b7ece9c66c4922f361
Dan Harris <dan@sparkcode.co.uk>
2019-03-12 23:26:22 +0000

    Cloudwatch logging

--- build.sh
+++ build.sh
@@ -5,3 +5,21 @@ set -e
 
 # Update system packages
 sudo yum update -y
+
+# Setup AWS CW logging
+sudo yum install -y awslogs
+
+sudo chown root:root \
+    ~ec2-user/awscli.conf \
+    ~ec2-user/awslogs.conf
+
+sudo chmod 640 \
+    ~ec2-user/awscli.conf \
+    ~ec2-user/awslogs.conf
+
+sudo mv -f \
+    ~ec2-user/awscli.conf \
+    ~ec2-user/awslogs.conf \
+    /etc/awslogs/
+
+sudo systemctl enable awslogsd
